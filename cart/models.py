e99f4b9d4d53dbc8088a1b43570f35121695590b
Codernirmal <codernirmal@gmail.com>
2025-03-25 14:05:04 +1100

    Store and Carts functionality

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from store.models import Product
+from store.models import Product,Variation
+
 
 # Create your models here.
 class Cart(models.Model):
@@ -10,6 +11,7 @@ class Cart(models.Model):
         return self.cart_id
 class CartItem(models.Model):
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    variations=models.ManyToManyField(Variation,blank=True)
     cart=models.ForeignKey(Cart,on_delete=models.CASCADE)
     quantity=models.IntegerField()
     is_active=models.BooleanField(default=True)
@@ -18,5 +20,5 @@ class CartItem(models.Model):
         return self.product.price * self.quantity
 
 
-    def __str__(self):
+    def __unicode__(self):
         return self.product
