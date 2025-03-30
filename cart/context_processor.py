b11ccbf09f173779e557ed78952329d53318f638
Codernirmal <codernirmal@gmail.com>
2025-03-30 12:17:26 +1100

    auth system done

--- cart/context_processor.py
+++ cart/context_processor.py
@@ -8,9 +8,11 @@ def counter(request):
         return {}
     else:
         try:
-            cart=Cart.objects.filter(cart_id=_cart_id(request)).first()
-          
-            cart_items=CartItem.objects.all().filter(cart=cart)
+            cart=Cart.objects.filter(cart_id=_cart_id(request))
+            if request.user.is_authenticated:
+                cart_items=CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_items = CartItem.objects.filter(cart=cart.first())
            
             for cart_item in cart_items:
                 cart_count+=cart_item.quantity
