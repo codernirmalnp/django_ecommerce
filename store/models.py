75916081e2a0ff6f5dc4c838cd4b8d250a264aa9
Codernirmal <codernirmal@gmail.com>
2025-04-07 14:24:53 +1000

    Review and rating system:

--- store/models.py
+++ store/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from category.models import Category
+from accounts.models import Account
 from django.urls import reverse
+from django.db.models import Avg, Count
+
 
 # Create your models here.
 class Product(models.Model):
@@ -15,15 +18,30 @@ class Product(models.Model):
     created_date=models.DateTimeField(auto_now_add=True)
     modified_date=models.DateTimeField(auto_now=True)
 
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
+
     def get_url(self):
         return reverse('product_detail',args=[self.category.slug,self.slug])
 
     def __str__(self):
         return self.product_name
+
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager,self).filter(variation_category='color',is_active=True)
-    def size(self):
+    def sizes(self):
           return super(VariationManager,self).filter(variation_category='size',is_active=True)
     
 variation_category_choice=(
@@ -40,3 +58,19 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+
+
+class ReviewRating(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    user=models.ForeignKey(Account,on_delete=models.CASCADE)
+    subject=models.CharField(max_length=100,blank=True)
+    review=models.TextField(max_length=500,blank=True)
+    rating=models.FloatField()
+    ip=models.CharField(max_length=20,blank=True)
+    status=models.BooleanField(default=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+    
