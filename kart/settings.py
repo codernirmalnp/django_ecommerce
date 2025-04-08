96fce9b45b1be1e8a1d433be2fb0b553b6e3561a
Codernirmal <codernirmal@gmail.com>
2025-04-08 15:17:22 +1000

    Postgres configuration

--- kart/settings.py
+++ kart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['http://ecommerce-env.eba-skxxq3jn.us-west-2.elasticbeanstalk.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
